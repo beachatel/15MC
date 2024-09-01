201909d3105cf6632bd7f3c15687bcfc49448191
Bea Chatel <bchatelbell@gmail.com>
2024-09-01 18:42:50 +0100

    Update

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  var map = L.map('map').setView([55.03878498763901, -1.4781508288359293], 13);
 
         // Load and display the tile layer from OpenStreetMap
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,6 +7,6 @@
         }).addTo(map);
 
         // Optional: Add a marker
-        var marker = L.marker([51.5, -0.09]).addTo(map)
+        var marker = L.marker([55.03878498763901, -1.4781508288359293]).addTo(map)
             .bindPopup('Your Location<br> 15MC')
             .openPopup();
